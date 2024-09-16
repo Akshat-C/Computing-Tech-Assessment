e4fa28a3932e5d59d99a39dbf1a783cad55ef693
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2024-09-16 20:10:09 +1000

    Add files via upload

--- games.js
+++ games.js
@@ -1,9 +1,24 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7qNaX-8a1PjKCGCJcAPo-9ZYurDGGHMc",
+    authDomain: "gamenet-assessment.firebaseapp.com",
+    databaseURL: "https://gamenet-assessment-default-rtdb.firebaseio.com",
+    projectId: "gamenet-assessment",
+    storageBucket: "gamenet-assessment.appspot.com",
+    messagingSenderId: "1090351654728",
+    appId: "1:1090351654728:web:f182a2b0854a2c5502b57d",
+    measurementId: "G-90TBFX7ZSF"
+  };
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const database = firebase.database(app);
+
 var game0 = {
     Genre: "adventure",
     Price: "39",
     Age: "10",
     Rating: "4",
-    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="game_title_games">Minecraft</h4><img src="Minecraft Display Image.jpeg" class="game_disp_img"><br></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity:</h5><span>3 Stars</span><br><br><h5>Price: $39.00</h5><br><a href="game_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
+    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn fa fa-heart-o" onclick="favourite(this)" id="Minecraft"></button><h4 class="game_title_games">Minecraft</h4><img src="Minecraft Display Image.jpeg" class="game_disp_img"><br></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity:</h5><span>3 Stars</span><br><br><h5>Price: $39.00</h5><br><a href="game_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
 };
 
 var game1 = {
@@ -11,7 +26,7 @@ var game1 = {
     Price: "0",
     Age: "0",
     Rating: "3",
-    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="game_title_games">Among Us</h4><img src="Amongus Display Image.jpeg" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity:</h4><span>2 Stars</span><br><br><h5>Price: Free</h5><br><a href="amongus_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
+    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn fa fa-heart-o" onclick="favourite(this)" id="Amongus"></button><h4 class="game_title_games">Among Us</h4><img src="Amongus Display Image.jpeg" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity:</h4><span>2 Stars</span><br><br><h5>Price: Free</h5><br><a href="amongus_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
 };
 
 
@@ -20,7 +35,7 @@ var game2 = {
     Price: "0",
     Age: "15",
     Rating: "4",
-    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="game_title_games">Fortnite</h4><img src="Fortnite Display Image.jpeg" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity: </h4><span>3 Stars</span><br><br><h5>Price: Free</h5><br><a href="fortnite_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
+    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn fa fa-heart-o" onclick="favourite(this)" id="Fortnite"></button><h4 class="game_title_games">Fortnite</h4><img src="Fortnite Display Image.jpeg" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity: </h4><span>3 Stars</span><br><br><h5>Price: Free</h5><br><a href="fortnite_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
 };
 
 var game3 = {
@@ -28,7 +43,7 @@ var game3 = {
     Price: "39",
     Age: "0",
     Rating: "5",
-    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="game_title_games">EA FC 24</h4><img src="FC24 Display Image.jpg" class="game_disp_img" ></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity:</h5><span>2 Stars</span><br><br><h5>Price: $39.00</h5><br><a href="fc24_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
+    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn" onclick="favourite(this)" id="FC24"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="game_title_games">EA FC 24</h4><img src="FC24 Display Image.jpg" class="game_disp_img" ></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity:</h5><span>2 Stars</span><br><br><h5>Price: $39.00</h5><br><a href="fc24_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
 };
 
 var game4 = {
@@ -36,7 +51,7 @@ var game4 = {
     Price: "0",
     Age: "0",
     Rating: "2",
-    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="game_title_games">Asphalt 9</h4><img src="Asphalt Display Image.jpeg" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity: </h5><span>3 Stars</span><br><br><h5>Price: $39.00</h5><br><a href="asphalt9_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
+    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn fa fa-heart-o" onclick="favourite(this)" id="Asphalt9"></button><h4 class="game_title_games">Asphalt 9</h4><img src="Asphalt Display Image.jpeg" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5>Popularity: </h5><span>3 Stars</span><br><br><h5>Price: $39.00</h5><br><a href="asphalt9_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
 };
 
 var game5 = {
@@ -44,7 +59,7 @@ var game5 = {
     Price: "124",
     Age: "15",
     Rating: "4.5",
-    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="col-12 game_title_games" style="margin-left: -10%;">God of War Ragnarok</h4><img src="Ragnarok Display.jpg" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5 style="margin-top: 8%;">Popularity: </h5><span>2 Stars</span><br><br><h5>Price: $124.95</h5><br><a href="ragnarok_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
+    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn fa fa-heart-o" onclick="favourite(this)" id="Ragnarok"></button><h4 class="col-12 game_title_games" style="margin-left: -10%;">God of War Ragnarok</h4><img src="Ragnarok Display.jpg" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center; margin-top: 5%;" class="col-6"><h5 style="margin-top: 8%;">Popularity: </h5><span>2 Stars</span><br><br><h5>Price: $124.95</h5><br><a href="ragnarok_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
 };
 
 var game6 = {
@@ -52,7 +67,7 @@ var game6 = {
     Price: "59.99",
     Age: "15",
     Rating: "4.5",
-    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="col-12 game_title_games">Elden Ring</h4><img src="Elden Display.webp" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center;" class="col-6"><h5 style="margin-top: 8%;">Popularity: </h5><span>2 Stars</span><br><br><h5>Price: $59.99</h5><br><a href="elden_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
+    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn fa fa-heart-o" onclick="favourite(this)" id="Elden"></button><h4 class="col-12 game_title_games">Elden Ring</h4><img src="Elden Display.webp" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center;" class="col-6"><h5 style="margin-top: 8%;">Popularity: </h5><span>2 Stars</span><br><br><h5>Price: $59.99</h5><br><a href="elden_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
 };
 
 var game7 = {
@@ -60,11 +75,12 @@ var game7 = {
     Price: "124",
     Age: "15",
     Rating: "4.5",
-    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn"><i class="fa fa-heart-o" aria-hidden="true"></i></button><h4 class="col-12 game_title_games" style="margin-left: -10%;">Red Dead Redemption 2</h4><img src="RDR2 Display.avif" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center;" class="col-6"><h5 style="margin-top: 25%;">Popularity: </h5><span>2 Stars</span><br><br><h5>Price: $90.95</h5><br><a href="rdr2_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
+    Data: '<div style="float: left;" class="col-6"><button class="btn icon_btn fa fa-heart-o" onclick="favourite(this)" id="RDR2"></button><h4 class="col-12 game_title_games" style="margin-left: -10%;">Red Dead Redemption 2</h4><img src="RDR2 Display.avif" class="game_disp_img"></div><div style="float: right; padding-top: 5%; text-align: center;" class="col-6"><h5 style="margin-top: 25%;">Popularity: </h5><span>2 Stars</span><br><br><h5>Price: $90.95</h5><br><a href="rdr2_detail.html"><button class="btn btn-primary">View Game</button></a></div>'
 };
 
 var all_games = [game0, game1, game2, game3, game4, game5, game6, game7];
 var all_displays = ["disp0", "disp1", "disp2", "disp3", "disp4", "disp5", "disp6", "disp7"];
+var all_faves = ["Minecraft", "Amongus", "Fortnite", "FC24", "Asphalt9", "Ragnarok", "Elden", "RDR2"];
 
 show_all();
 
@@ -84,6 +100,7 @@ function show_all()
                 }
                 i++;
             }
+            checkIfFavorited();
         });
     }
     console.log("Show_all occurred");
@@ -160,3 +177,81 @@ function filter() {
     // Update the display with the number of matching games
     //document.getElementById('match-count').textContent = `Matching Games: ${matchCount}`;
 }
+
+var username = localStorage.getItem("Email: ");
+
+if (username != "")
+{
+var userFavoritesRef = firebase.database().ref("User_credentials/" + username + "/Favourites");
+}
+
+function favourite(game_element) {
+    // First, check if the game is already in the favorites list
+    game_name = game_element.id;
+    fave_element = document.getElementById(game_name);
+if (username != "")
+{
+    userFavoritesRef.once('value', function(snapshot) {
+        let foundKey = null;
+        snapshot.forEach(function(childSnapshot) {
+            if (childSnapshot.val() === game_name) {
+                foundKey = childSnapshot.key; // Store the key if found
+            }
+        });
+
+        // If the game is found, remove it from favorites
+        if (foundKey) {
+            userFavoritesRef.child(foundKey).remove()
+            .then(() => {
+                console.log("Game removed from favorites:", game_name);
+                fave_element.classList.remove("fa-heart");
+                fave_element.classList.add("fa-heart-o");
+            })
+            .catch((error) => {
+                console.error("Error removing favorite:", error);
+            });
+        } 
+        // If the game is not found, add it to favorites
+        else {
+            userFavoritesRef.push(game_name)
+            .then(() => {
+                console.log("Game added to favorites:", game_name);
+                fave_element.classList.remove("fa-heart-o");
+                fave_element.classList.add("fa-heart");
+            })
+            .catch((error) => {
+                console.error("Error adding favorite:", error);
+            });
+        }
+    });
+}
+}
+
+// Function to check if a game is favorited on page load
+function checkIfFavorited() {
+    userFavoritesRef.once('value', function(snapshot) {
+        let isFavorited = false;
+        let i = 0;
+        snapshot.forEach(function(childSnapshot) {
+            game_name = childSnapshot.val();
+            if (all_faves.includes(game_name)) {
+                isFavorited = true;
+                fave_element = document.getElementById(game_name);
+                fave_element.classList.remove("fa-heart-o");
+                fave_element.classList.add("fa-heart");
+            }
+            i++;
+        });
+        // Adjust button state based on whether it's favorited or not
+        /*if (isFavorited) {
+            
+        }*/
+    });
+}
+
+// Call this function when the page loads to check the initial button state
+document.addEventListener("DOMContentLoaded", function() {
+    
+
+    // Add event listener to toggle favorite status when clicked
+});
